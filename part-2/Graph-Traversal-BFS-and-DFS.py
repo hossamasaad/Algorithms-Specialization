a7d1573df8910a21b019aee8c44e6d113c3bfbfd
hossamasaad <40635600+hossamasaad@users.noreply.github.com>
2020-03-16 18:59:41 +0200

    Create Graph-Traversal-BFS-and-DFS.py

---
+++ part-2/Graph-Traversal-BFS-and-DFS.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Build your depth-first search function below:
+def bfs(graph, start_vertex, target_value):
+  path = [start_vertex]
+  vertex_and_path = [start_vertex, path]
+  bfs_queue = [vertex_and_path]
+  visited = set()
+  while bfs_queue:
+    current_vertex, path = bfs_queue.pop(0)
+    visited.add(current_vertex)
+    for neighbor in graph[current_vertex]:
+      if neighbor not in visited:
+        if neighbor is target_value:
+          return path + [neighbor]
+        else:
+          bfs_queue.append([neighbor, path + [neighbor]])
+
+def dfs(graph, current_vertex, target_value, visited = None):
+  if visited is None:
+    visited = []
+  visited.append(current_vertex)
+  if current_vertex is target_value:
+    return visited
+  
+  for neighbor in graph[current_vertex]:
+    if neighbor not in visited:
+      path = dfs(graph, neighbor, target_value, visited)
+      if path:
+        return path
+
+
+some_important_graph = {
+    'lava': set(['sharks', 'piranhas']),
+    'sharks': set(['lava', 'bees', 'lasers']),
+    'piranhas': set(['lava', 'crocodiles']),
+    'bees': set(['sharks']),
+    'lasers': set(['sharks', 'crocodiles']),
+    'crocodiles': set(['piranhas', 'lasers'])
+  }
+
+  
+  
+if __name__ == "__main__":
+    print(bfs(some_important_graph, 'lava', 'lasers'))
+    print(dfs(some_important_graph, 'lava', 'lasers'))
+    
+    
